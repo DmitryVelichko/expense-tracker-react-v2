9fe191c7ff3a07513ab53567ab6366e396b751f6
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-22 23:14:39 +0300

    new commit

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -2,15 +2,13 @@ import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
-  const context = useContext(GlobalContext)
+  const {transactions} = useContext(GlobalContext)
   
   return (
     <>
     <h3>History</h3>
     <ul className="list">
-        <li className="minus">
-            Cash <span>-$400</span><button className='delete-btn'>x</button>
-        </li>
+        {transactions.map(transaction => ())}
     </ul>
     </>
   )
