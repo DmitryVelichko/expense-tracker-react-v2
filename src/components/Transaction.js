0fd7c310e145587ec6cd8091792cecb08347be91
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-23 22:37:46 +0300

    new commit

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
 const Transaction = ({transaction}) => {
+  const sign = transaction.amount < 0 ? '-' : '+';
+
   return (
     <li className='minus'>
       {transaction.text} <span>-$400</span>
