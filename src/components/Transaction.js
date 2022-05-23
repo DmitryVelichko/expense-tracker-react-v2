09b452bf61f4a39c4a039d811beb8526a6ad7425
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-23 22:48:20 +0300

    new commit

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { GlobalContext } from '../context/GlobalState'
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 //Money formatter function
 function moneyFormatter(num) {
@@ -17,18 +17,14 @@ function moneyFormatter(num) {
   );
 }
 
-const Transaction = ({transaction}) => {
-
+export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
 
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
     <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
-      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-      <button className='delete-btn'>x</button>
+      {transaction.text} <span>{sign}{moneyFormatter(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
-  );
-};
-
-export default Transaction;
+  )
+}
