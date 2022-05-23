2deeecf4d22e88dad6c8265be0e68a46355feca3
Dmitry Velichko <soleil-2005@mail.ru>
2022-05-23 22:49:21 +0300

    new commit

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,18 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { Transaction } from './Transaction';
 
-const IncomeExpenses = () => {
+import { GlobalContext } from '../context/GlobalState';
+
+export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
-    <div className='inc-exp-container'>
-        <div>
-            <h4>Income</h4>
-            <p className='money plus'>+$0.00</p>
-        </div>
-        <div>
-            <h4>Expense</h4>
-            <p className='money minus'>-$0.00</p>
-        </div>
-    </div>
+    <>
+      <h3>History</h3>
+      <ul className="list">
+        {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+      </ul>
+    </>
   )
 }
-
-export default IncomeExpenses
